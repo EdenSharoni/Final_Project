2b0c6f39d714eb6d169e7c5881f05b498c1efaa0
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-07-01 10:37:08 +0300

    Sound

--- Final_Project/Assets/Scripts/PacManScript.cs
+++ Final_Project/Assets/Scripts/PacManScript.cs
@@ -6,57 +6,68 @@ public class PacManScript : MonoBehaviour
 {
     public float speed = 1f;
     AudioSource audioSource;
-    public AudioClip audio;
+    public AudioClip audioClip;
+    public AudioClip wakkawakka;
+    bool afterAudio;
 
     private void Start()
     {
+        afterAudio = false;
         audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(audio);
+        audioSource.PlayOneShot(audioClip);
         StartCoroutine(Wait());
     }
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(audio.length);
+        yield return new WaitForSeconds(audioClip.length);
+        afterAudio = true;
         GetComponent<Animator>().SetBool("right", true);
+        audioSource.loop = true;
+        audioSource.clip = wakkawakka;
+        audioSource.Play();
     }
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) ||GetComponent<Animator>().GetBool("right"))
+        if (afterAudio)
         {
-            GetComponent<Animator>().SetBool("up", false);
-            GetComponent<Animator>().SetBool("down", false);
-            GetComponent<Animator>().SetBool("left", false);
-            GetComponent<Animator>().SetBool("right", true);
-            transform.Translate( speed * Time.deltaTime, 0, 0);
+            if (Input.GetKeyDown(KeyCode.RightArrow) || GetComponent<Animator>().GetBool("right"))
+            {
+                GetComponent<Animator>().SetBool("up", false);
+                GetComponent<Animator>().SetBool("down", false);
+                GetComponent<Animator>().SetBool("left", false);
+                GetComponent<Animator>().SetBool("right", true);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || GetComponent<Animator>().GetBool("left"))
+            {
+                GetComponent<Animator>().SetBool("up", false);
+                GetComponent<Animator>().SetBool("down", false);
+                GetComponent<Animator>().SetBool("right", false);
+                GetComponent<Animator>().SetBool("left", true);
+                transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) || GetComponent<Animator>().GetBool("down"))
+            {
+                GetComponent<Animator>().SetBool("up", false);
+                GetComponent<Animator>().SetBool("right", false);
+                GetComponent<Animator>().SetBool("left", false);
+                GetComponent<Animator>().SetBool("down", true);
+                transform.Translate(0, -1 * speed * Time.deltaTime, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || GetComponent<Animator>().GetBool("up"))
+            {
+                GetComponent<Animator>().SetBool("right", false);
+                GetComponent<Animator>().SetBool("left", false);
+                GetComponent<Animator>().SetBool("down", false);
+                GetComponent<Animator>().SetBool("up", true);
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow)||GetComponent<Animator>().GetBool("left"))
-        {
-            GetComponent<Animator>().SetBool("up", false);
-            GetComponent<Animator>().SetBool("down", false);
-            GetComponent<Animator>().SetBool("right", false);
-            GetComponent<Animator>().SetBool("left", true);
-            transform.Translate(-1* speed * Time.deltaTime, 0, 0);
-        }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) || GetComponent<Animator>().GetBool("down"))
-        {
-            GetComponent<Animator>().SetBool("up", false);
-            GetComponent<Animator>().SetBool("right", false);
-            GetComponent<Animator>().SetBool("left", false);
-            GetComponent<Animator>().SetBool("down", true);
-            transform.Translate(0, -1* speed * Time.deltaTime, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) || GetComponent<Animator>().GetBool("up"))
-        {
-            GetComponent<Animator>().SetBool("right", false);
-            GetComponent<Animator>().SetBool("left", false);
-            GetComponent<Animator>().SetBool("down", false);
-            GetComponent<Animator>().SetBool("up", true);
-            transform.Translate(0, speed * Time.deltaTime, 0);
-        }
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -66,8 +77,8 @@ public class PacManScript : MonoBehaviour
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("teleport1"))
-            transform.Translate(-60, 0,0);
+            transform.Translate(-60, 0, 0);
         if (collision.gameObject.CompareTag("teleport2"))
-            transform.Translate(60, 0,0);
+            transform.Translate(60, 0, 0);
     }
 }
