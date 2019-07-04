5e7a23fcf020bdaa85bd6f0f005f1e5aaacee808
Barel3220 <42840215+Barel3220@users.noreply.github.com>
2019-07-04 16:34:37 +0300

    3D has started

---
+++ Final_Project/Assets/3D/Scripts/PacmanControllerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanControllerScript : MonoBehaviour
+{
+    public float movementSpeed = 0f;
+
+    private Animator animator = null;
+
+    private Vector3 up = Vector3.zero,
+                    right = new Vector3(0, 90, 0),
+                    down = new Vector3(0, 180, 0),
+                    left = new Vector3(0, 270, 0),
+                    currentDirection = Vector3.zero;
+
+    private Vector3 initialPosition = Vector3.zero;
+
+    public void Reset()
+    {
+        transform.position = initialPosition;
+        animator.SetBool("isDead", false);
+        animator.SetBool("isMoving", false);
+        currentDirection = down;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+
+        initialPosition = transform.position;
+
+        animator = GetComponent<Animator>();
+
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isMoving = true;
+        bool isDead = animator.GetBool("isDead");
+
+        if (isDead) isMoving = false;
+        else if (Input.GetKey(KeyCode.UpArrow)) currentDirection = up;
+        else if (Input.GetKey(KeyCode.RightArrow)) currentDirection = right;
+        else if (Input.GetKey(KeyCode.DownArrow)) currentDirection = down;
+        else if (Input.GetKey(KeyCode.LeftArrow)) currentDirection = left;
+        else isMoving = false;
+
+        transform.localEulerAngles = currentDirection;
+
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ghost3D"))
+            animator.SetBool("isDead", true);
+    }
+}
