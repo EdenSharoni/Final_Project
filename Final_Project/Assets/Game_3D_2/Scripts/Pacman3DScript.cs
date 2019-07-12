07cbc507ed5869a47559770f60a89e489d10ea50
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-07-12 11:09:46 +0300

    Dots and Ghost in 3D new game

--- Final_Project/Assets/Game_3D_2/Scripts/Pacman3DScript.cs
+++ Final_Project/Assets/Game_3D_2/Scripts/Pacman3DScript.cs
@@ -10,16 +10,30 @@ public class Pacman3DScript : MonoBehaviour
     public AudioClip deadSound;
     Vector3 startPoint;
     public bool ghostBlue;
-    float speed = 10f;
+    float speed = 0f;
     public Rigidbody rb;
     public bool isdead;
+    int directionX;
+    int directionY;
+    bool anotherDot;
+
+
+    private Vector3 up = Vector3.zero,
+                    right = new Vector3(0, 90, 0),
+                    down = new Vector3(0, 180, 0),
+                    left = new Vector3(0, 270, 0),
+                    currentDirection = Vector3.zero;
+    private NavMeshScript agent;
 
     void Start()
     {
+        currentDirection = down;
+        rb = GetComponent<Rigidbody>();
         startPoint = transform.position;
         audioSource = GetComponent<AudioSource>();
         initPacman();
     }
+
     public void initPacman()
     {
         StartCoroutine(Wait());
@@ -27,28 +41,70 @@ public class Pacman3DScript : MonoBehaviour
 
     IEnumerator Wait()
     {
+        speed = 0;
         transform.position = startPoint;
         audioSource.PlayOneShot(audioClip);
-        rb.constraints = RigidbodyConstraints.FreezeAll;
+        //rb.constraints = RigidbodyConstraints.FreezeAll;
         yield return new WaitForSeconds(audioClip.length);
         startPacman();
     }
 
     void startPacman()
     {
+        speed = 10f;
+        anotherDot = true;
         ghostBlue = false;
         isdead = false;
         GetComponent<Animator>().SetBool("move", true);
-        //directionX = 1;
-        //directionY = 0;
+        directionX = 1;
+        directionY = 0;
         transform.eulerAngles = new Vector3(0, 0, 0);
 
-        rb.constraints = RigidbodyConstraints.None;
-        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //rb.constraints = RigidbodyConstraints.None;
+    }
+    private void Update()
+    {
+        GetInput();
     }
 
-    void Update()
+    void GetInput()
     {
-        
+        if (!isdead)
+        {
+            if (Input.GetKey(KeyCode.UpArrow)) currentDirection = up;
+            else if (Input.GetKey(KeyCode.RightArrow)) currentDirection = right;
+            else if (Input.GetKey(KeyCode.DownArrow)) currentDirection = down;
+            else if (Input.GetKey(KeyCode.LeftArrow)) currentDirection = left;
+        }
+        transform.localEulerAngles = currentDirection;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("powerupDot"))
+        {
+            ghostBlue = true;
+            if (anotherDot)
+                audioSource.PlayOneShot(wakkawakka);
+            StartCoroutine(WaitForSoundToEnd());
+            PlayerPrefs.SetInt("points", PlayerPrefs.GetInt("points") + 50);
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Food3D"))
+        {
+            if (anotherDot)
+                StartCoroutine(WaitForSoundToEnd());
+            PlayerPrefs.SetInt("points", PlayerPrefs.GetInt("points") + 10);
+            Destroy(other.gameObject);
+        }
+    }
+
+    IEnumerator WaitForSoundToEnd()
+    {
+        anotherDot = false;
+        audioSource.PlayOneShot(wakkawakka);
+        yield return new WaitForSeconds(wakkawakka.length);
+        anotherDot = true;
     }
 }
