86f29d2de260dc7fcf8dfea8af221993876a1e58
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-07-15 01:07:18 +0300

    3 Cameras Set

---
+++ Final_Project/Assets/Game_3D_2/Scripts/AboveCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboveCameraScript : MonoBehaviour
+{
+    public Transform playerObject;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = .5f;
+
+    void Start()
+    {
+        cameraOffset = transform.position - playerObject.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = playerObject.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+    }
+}
