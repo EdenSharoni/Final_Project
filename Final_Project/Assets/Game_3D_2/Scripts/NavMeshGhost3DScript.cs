293711c60852f92519f624c21d0f2775b03071e1
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-07-14 18:36:16 +0300

    ghost move randomly

--- Final_Project/Assets/Game_3D_2/Scripts/NavMeshGhost3DScript.cs
+++ Final_Project/Assets/Game_3D_2/Scripts/NavMeshGhost3DScript.cs
@@ -6,14 +6,15 @@ using UnityEngine.AI;
 public class NavMeshGhost3DScript : MonoBehaviour
 {
     public Transform target;
+    public Transform wayPoint;
+    public Transform wayPoint2;
     private NavMeshAgent agent;
     Pacman3DScript pacman;
     Vector3 startPosition;
-    Vector3[] directions = new Vector3[4];
     private Material material;
     private Color original;
     bool oneTimeBlue;
-
+    float speed = 0f;
     RaycastHit forward;
     RaycastHit backwards;
     RaycastHit left;
@@ -27,19 +28,26 @@ public class NavMeshGhost3DScript : MonoBehaviour
     List<int> checkLoopDirections = new List<int>();
     string lastdirection;
     int rand;
-
+    Vector3[] directions = new Vector3[4];
+    bool oneTimeEntrence;
+    Vector3 moveDirection;
+    bool gotowaypoint2;
+    bool startFindingPacman;
     void Start()
     {
+        startFindingPacman = false;
+        gotowaypoint2 = false;
+        oneTimeEntrence = true;
+        directions[0] = Vector3.forward;
+        directions[1] = Vector3.right;
+        directions[2] = Vector3.back;
+        directions[3] = Vector3.left;
+
         pacman = GameObject.Find("Pacman3D").GetComponent<Pacman3DScript>();
         agent = GetComponent<NavMeshAgent>();
         startPosition = transform.position;
 
         material = GetComponent<Renderer>().material;
-
-        directions[0] = Vector3.forward;
-        directions[1] = Vector3.right;
-        directions[2] = Vector3.back;
-        directions[3] = Vector3.left;
         StartCoroutine(WaitInit());
 
     }
@@ -61,7 +69,7 @@ public class NavMeshGhost3DScript : MonoBehaviour
         if (transform.name.Equals("GreenGhost"))
             yield return new WaitForSeconds(3f);
         if (transform.name.Equals("DarkGreenGhost"))
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(1f);
         if (transform.name.Equals("PinkGhost"))
             yield return new WaitForSeconds(8f);
         if (transform.name.Equals("LightBlueGhost"))
@@ -71,16 +79,44 @@ public class NavMeshGhost3DScript : MonoBehaviour
         if (transform.name.Equals("PurpleGhost"))
             yield return new WaitForSeconds(15f);
         agent.speed = 8;
+        speed = 10f;
     }
 
     void FixedUpdate()
     {
-        FindRayCastHit();
 
         if (pacman.ghostBlue && oneTimeBlue)
             StartCoroutine(Blue());
 
-        agent.SetDestination(target.position);
+        if (transform.position == wayPoint2.position)
+        {
+            GetComponent<CapsuleCollider>().isTrigger = false;
+            startFindingPacman = true;
+        }
+            
+
+        //agent.SetDestination(target.position);
+
+        if (speed == 10f && !startFindingPacman)
+            GetOutOfHome();
+
+        if (startFindingPacman)
+            Algorithm();
+
+        if (moveDirection != null)
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+    void GetOutOfHome()
+    {
+        GetComponent<CapsuleCollider>().isTrigger = true;
+        Vector3 p1 = Vector3.MoveTowards(transform.position, wayPoint.position, speed * Time.deltaTime);
+        GetComponent<Rigidbody>().MovePosition(p1);
+        if (transform.position.z == p1.z)
+        {
+            Vector3 p2 = Vector3.MoveTowards(transform.position, wayPoint2.position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(p2);
+        }
     }
 
     void Algorithm()
@@ -90,6 +126,11 @@ public class NavMeshGhost3DScript : MonoBehaviour
             finishWaiting = false;
 
             FindRayCastHit();
+
+            /*Debug.Log("forward: " + freeDirection[0]);
+            Debug.Log("right: " + freeDirection[1]);
+            Debug.Log("back: " + freeDirection[2]);
+            Debug.Log("left: " + freeDirection[3]);*/
 
             for (int i = 0; i < 4; i++)
             {
@@ -116,38 +157,9 @@ public class NavMeshGhost3DScript : MonoBehaviour
                 else
                     rand = Random.Range(0, counter);
                 checkLoopDirections.Add(options[rand]);
-                //lastdirection = directions[options[rand]];
-                Switches(lastdirection);
+                moveDirection = directions[options[rand]];
             }
             StartCoroutine(Wait());
-        }
-    }
-
-    void Switches(string s)
-    {
-        GetComponent<Animator>().SetBool("up", false);
-        GetComponent<Animator>().SetBool("down", false);
-        GetComponent<Animator>().SetBool("left", false);
-        GetComponent<Animator>().SetBool("right", false);
-
-        switch (s)
-        {
-            case "right":
-                lastdirection = "right";
-                GetComponent<Animator>().SetBool("right", true);
-                break;
-            case "left":
-                lastdirection = "left";
-                GetComponent<Animator>().SetBool("left", true);
-                break;
-            case "up":
-                lastdirection = "up";
-                GetComponent<Animator>().SetBool("up", true);
-                break;
-            case "down":
-                lastdirection = "down";
-                GetComponent<Animator>().SetBool("down", true);
-                break;
         }
     }
 
@@ -160,6 +172,70 @@ public class NavMeshGhost3DScript : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         finishWaiting = true;
     }
+
+    void FindRayCastHit()
+    {
+
+        if (Physics.SphereCast(transform.position, 1.5f, Vector3.forward, out forward, 3f, layermask))
+        {
+            if (freeDirection[0] == true)
+                oneTimeDirection = true;
+            freeDirection[0] = false;
+        }
+        else
+        {
+            if (freeDirection[0] == false)
+                oneTimeDirection = true;
+            freeDirection[0] = true;
+        }
+        if (Physics.SphereCast(transform.position, 1.5f, Vector3.right, out right, 3f, layermask))
+        {
+            if (freeDirection[1] == true)
+                oneTimeDirection = true;
+            freeDirection[1] = false;
+        }
+        else
+        {
+            if (freeDirection[1] == false)
+                oneTimeDirection = true;
+            freeDirection[1] = true;
+        }
+        if (Physics.SphereCast(transform.position, 1.5f, Vector3.back, out backwards, 3f, layermask))
+        {
+            if (freeDirection[2] == true)
+                oneTimeDirection = true;
+            freeDirection[2] = false;
+        }
+        else
+        {
+            if (freeDirection[2] == false)
+                oneTimeDirection = true;
+            freeDirection[2] = true;
+        }
+
+        if (Physics.SphereCast(transform.position, 1.5f, Vector3.left, out left, 3f, layermask))
+        {
+            if (freeDirection[3] == true)
+                oneTimeDirection = true;
+            freeDirection[3] = false;
+        }
+        else
+        {
+            if (freeDirection[3] == false)
+                oneTimeDirection = true;
+            freeDirection[3] = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + Vector3.forward, 1.5f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.right, 1.5f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.back, 1.5f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.left, 1.5f);
+    }
+
     IEnumerator Blue()
     {
         oneTimeBlue = false;
@@ -168,30 +244,5 @@ public class NavMeshGhost3DScript : MonoBehaviour
         pacman.ghostBlue = false;
         material.color = original;
         oneTimeBlue = true;
-    }
-
-    void FindRayCastHit()
-    {
-        Debug.DrawRay(transform.position, Vector3.forward * 5f, Color.red);
-        Debug.DrawRay(transform.position, Vector3.back * 5f, Color.red);
-        Debug.DrawRay(transform.position, Vector3.right * 5f, Color.red);
-        Debug.DrawRay(transform.position, Vector3.left * 5f, Color.red);
-
-        if (Physics.Raycast(transform.position, Vector3.forward * 5f, out forward, 5, layermask))
-        {
-            Debug.Log(forward.collider.tag);
-        }
-        else if (Physics.Raycast(transform.position, Vector3.forward * 5f, out backwards, 5, layermask))
-        {
-            Debug.Log(forward.collider.tag);
-        }
-        else if (Physics.Raycast(transform.position, Vector3.forward * 5f, out right, 5, layermask))
-        {
-            Debug.Log(forward.collider.tag);
-        }
-        else if (Physics.Raycast(transform.position, Vector3.forward * 5f, out left, 5, layermask))
-        {
-            Debug.Log(forward.collider.tag);
-        }
     }
 }
