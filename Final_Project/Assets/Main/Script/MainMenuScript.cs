8b8c86a15807f2dd793caadb411b860862f30a98
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-07-16 13:22:24 +0300

    mainmenu

--- Final_Project/Assets/Main/Script/MainMenuScript.cs
+++ Final_Project/Assets/Main/Script/MainMenuScript.cs
@@ -2,19 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public AudioSource source;
+    public Button howToPlayBtn;
+    public Button difficultyBtn;
+    public Button game1;
+    public Button game2;
+    public Button game3;
+    public RawImage trailer;
+    public VideoPlayer videoPlayer;
+    public GameObject panel;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        howToPlayBtn.enabled = true;
+        howToPlayBtn.gameObject.SetActive(true);
+
+        difficultyBtn.enabled = true;
+        difficultyBtn.gameObject.SetActive(true);
+
+        game1.enabled = false;
+        game1.gameObject.SetActive(false);
+
+        game2.enabled = false;
+        game2.gameObject.SetActive(false);
+
+        game3.enabled = false;
+        game3.gameObject.SetActive(false);
+
+        trailer.enabled = true;
+        trailer.gameObject.SetActive(true);
+
+        panel.gameObject.SetActive(false);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Video()
     {
-        
+        StartCoroutine(PlayVideo());
     }
+
+    IEnumerator PlayVideo()
+    {
+        Debug.Log("aaa");
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(1f);
+            break;
+        }
+        trailer.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        source.Play();
+    }
+
+    public void HowToPlay()
+    {
+        panel.gameObject.SetActive(!panel.gameObject.activeSelf);
+        difficultyBtn.enabled = !difficultyBtn.enabled;
+        difficultyBtn.gameObject.SetActive(!difficultyBtn.gameObject.activeSelf);
+    }
+
 }
